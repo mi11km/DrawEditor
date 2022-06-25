44651607b379c7010e8112ff42760ace9e33a3d7
mi11km <mi11km.basket@gmail.com>
2022-06-25 18:17:36 +0900

    refactoring

--- app/src/main/java/DrawEditor/App.java
+++ app/src/main/java/DrawEditor/App.java
@@ -3,8 +3,30 @@
  */
 package DrawEditor;
 
-public class App {
+import DrawEditor.Controller.Controllers;
+import DrawEditor.Model.DrawModel;
+import DrawEditor.View.ViewPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JFrame {
+
+  public App() {
+    this.setBackground(Color.white);
+    this.setTitle("Draw Editor");
+    this.setSize(1000, 1000);
+
+    DrawModel model = new DrawModel();
+    Controllers controllers = new Controllers(model);
+    ViewPanel view = new ViewPanel(model, controllers);
+    this.add(view);
+
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+  }
+
   public static void main(String[] args) {
-    new DrawFrame();
+    new App();
   }
 }
